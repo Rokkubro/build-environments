abab31324a08bb684e49d2984ee9c1dde68c10b1
Rokkubro <lachlanb03@gmail.com>
2023-06-01 20:37:52 +1000

    Switch to forked repo and dockerhub
    Login and push on clang branch
    
    Install clang on Linux-fresh
    
    Install lld on linux-fresh
    
    Add clang and lld to linux-appimage
    
    Use older clang and lld
    
    Download clang for mingw
    
    Dereference links and copy straight to /usr; also copy binaries required by clang
    
    Cleanup
    
    More cleanup
    
    Revert github.ref and github.repository for merge
    
    also revert to original docker hub repo

--- linux-mingw/mingw-setup.sh
+++ linux-mingw/mingw-setup.sh
@@ -15,6 +15,19 @@ cp -v "${FILENAME}"/bin/*.dll /usr/x86_64-w64-mingw32/lib/
 cp -vr "${FILENAME}"/include /usr/x86_64-w64-mingw32/
 cp -v "${FILENAME}"/lib/*.{a,def} /usr/x86_64-w64-mingw32/lib/
 
+# clang
+CLANG_VER='16.0.5'
+LINK_PATH="20230603/llvm-mingw-20230603-ucrt-ubuntu-20.04-x86_64"
+FILENAME="${LINK_PATH##*/}"
+echo "Downloading clang (${CLANG_VER})..."
+wget -c "https://github.com/mstorsjo/llvm-mingw/releases/download/${LINK_PATH}.tar.xz"
+tar -xf "${FILENAME}.tar.xz"
+
+echo "Copying Clang ${CLANG_VER} files to sysroot..."
+cp -aLv "${FILENAME}"/* /usr/
+echo "Copying required Clang binaries to sysroot..."
+cd /clang64
+find . -name \*.dll\* -exec cp -prv --parents {} /usr/x86_64-w64-mingw32/ \;
 # remove the directory
 ABS_PATH="$(readlink -f $0)"
 rm -rf "$(dirname ${ABS_PATH})"
